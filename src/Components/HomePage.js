c489aa7f180a4745e34fec7ca97f859e6d55b79d
dharmishshah <shah.dharm@husky.neu.edu>
2020-04-17 01:43:18 -0400

    portfolio commit first

---
+++ src/Components/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/App.css';
+
+function HomePage() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello World</p>
+            </header>
+        </div>
+    );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HomePage from "./Components/HomePage";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
