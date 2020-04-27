6760f89c2d289040b8c599337a19f9f215b8363a
dharmishshah <shah.dharm@husky.neu.edu>
2020-04-27 18:12:39 -0400

    adding views

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@
 //export const IP_ADDRESS = 'http://localhost:8080'
 
 //heroku
-export const IP_ADDRESS = 'https://dharmishshahdjango.herokuapp.com'
-//export const IP_ADDRESS = 'http://localhost:8000'
+//export const IP_ADDRESS = 'https://dharmishshahdjango.herokuapp.com'
+export const IP_ADDRESS = 'http://localhost:8000'
 
 //export const IP_ADDRESS = 'https://memebook.herokuapp.com'
