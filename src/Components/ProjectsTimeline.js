d4acc0e9cf6138cd626bcf6d56aff12e9ae02526
Dharmish Shah <shah.dharm@husky.neu.edu>
2020-06-07 21:07:47 -0400

    changes

--- src/Components/ProjectsTimeline.js
+++ src/Components/ProjectsTimeline.js
@@ -5,8 +5,8 @@ import cookie from "react-cookies";
 import '../css/styles.css';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-// import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-// import 'react-vertical-timeline-component/style.min.css';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import dateFormat from 'dateformat'
 
 export default class EducationRow extends Component {
@@ -44,32 +44,32 @@ export default class EducationRow extends Component {
         return (
             <div>
 
-                {/*<VerticalTimeline className="vertical-timeline-custom-line">*/}
-                {/*{this.state.projects.map(project => {*/}
-                {/*    return (*/}
-                {/*            <VerticalTimelineElement*/}
-                {/*                className="vertical-timeline-element--work"*/}
-                {/*                contentStyle={{ background: '#2c3e50' , color: '#fff' }}*/}
-                {/*                contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}*/}
-                {/*                date={dateFormat(project.fields.dateFormat, "mmmm, yyyy")}*/}
-                {/*                dateClassName="dateContent"*/}
-                {/*                iconStyle={{ background: '#2c3e50', color: '#fff' }}*/}
-                {/*            >*/}
-                {/*                <h3 className="vertical-timeline-element-title">{project.fields.name}</h3>*/}
-                {/*                /!*<h4 className="vertical-timeline-element-subtitle">{project.fields.city},*!/*/}
-                {/*                /!*    {project.fields.state_abbr}</h4>*!/*/}
+                <VerticalTimeline className="vertical-timeline-custom-line">
+                {this.state.projects.map(project => {
+                    return (
+                            <VerticalTimelineElement
+                                className="vertical-timeline-element--work"
+                                contentStyle={{ background: '#2c3e50' , color: '#fff' }}
+                                contentArrowStyle={{ borderRight: '7px solid  #2c3e50' }}
+                                date={dateFormat(project.fields.dateFormat, "mmmm, yyyy")}
+                                dateClassName="dateContent"
+                                iconStyle={{ background: '#2c3e50', color: '#fff' }}
+                            >
+                                <h3 className="vertical-timeline-element-title">{project.fields.name}</h3>
+                                {/*<h4 className="vertical-timeline-element-subtitle">{project.fields.city},*/}
+                                {/*    {project.fields.state_abbr}</h4>*/}
 
-                {/*                    {project.fields.description.split("\n").map(descrip => {*/}
-                {/*                        return (*/}
-                {/*                            <li className="description-font">*/}
-                {/*                                {descrip}*/}
-                {/*                            </li>*/}
-                {/*                        )*/}
-                {/*                    })}*/}
-                {/*            </VerticalTimelineElement>*/}
-                {/*    )*/}
-                {/*})}*/}
-                {/*</VerticalTimeline>*/}
+                                    {project.fields.description.split("\n").map(descrip => {
+                                        return (
+                                            <li className="description-font">
+                                                {descrip}
+                                            </li>
+                                        )
+                                    })}
+                            </VerticalTimelineElement>
+                    )
+                })}
+                </VerticalTimeline>
             </div>
         )
     }
